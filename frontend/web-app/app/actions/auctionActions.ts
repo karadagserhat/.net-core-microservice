149c24930f091dfdac151a5ed40a8a7c4a2ece04
karadagserhat <serhatkaradagg@outlook.com>
2025-03-23 14:01:42 +0300

    section 9 - client side identity with NextAuth

--- frontend/web-app/app/actions/auctionActions.ts
+++ frontend/web-app/app/actions/auctionActions.ts
@@ -1,6 +1,7 @@
 'use server';
 
 import { Auction, PagedResult } from '@/types';
+import { auth } from '@/auth';
 
 export async function getData(query: string): Promise<PagedResult<Auction>> {
   const res = await fetch(`http://localhost:6001/search${query} `);
@@ -8,4 +9,30 @@ export async function getData(query: string): Promise<PagedResult<Auction>> {
   if (!res.ok) throw new Error('Failed to fetch data');
 
   return res.json();
+}
+
+export async function updateAuctionTest(): Promise<
+  string | { status: number; message: string }
+> {
+  const data = {
+    mileage: Math.floor(Math.random() * 10000) + 1,
+  };
+
+  const session = await auth();
+
+  const res = await fetch(
+    'http://localhost:6001/auctions/afbee524-5972-4075-8800-7d1f9d7b0a0c',
+    {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json',
+        Authorization: 'Bearer ' + session?.accessToken,
+      },
+      body: JSON.stringify(data),
+    }
+  );
+
+  if (!res.ok) return { status: res.status, message: res.statusText };
+
+  return res.statusText;
 }
--- frontend/web-app/app/nav/Logo.tsx
+++ frontend/web-app/app/nav/Logo.tsx
@@ -3,13 +3,23 @@
 import { useParamsStore } from '@/hooks/useParamsStore';
 import React from 'react';
 import { AiOutlineCar } from 'react-icons/ai';
+import { usePathname } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 
 export default function Logo() {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  function doReset() {
+    if (pathname !== '/') router.push('/');
+    reset();
+  }
+
   const reset = useParamsStore((state) => state.reset);
 
   return (
     <div
-      onClick={reset}
+      onClick={doReset}
       className="flex items-center gap-2 text-3xl font-semibold text-red-500 cursor-pointer"
     >
       <AiOutlineCar size={34} />
